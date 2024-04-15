6765dc70f15b13552b93855d36a758edc216b182
Adrian Marstokk <adrian.marstokk@gmail.com>
2024-04-15 13:55:37 +0200

    charger component almost done

--- src/components/charger/run.py
+++ src/components/charger/run.py
@@ -4,48 +4,37 @@ from stmpy import Machine, Driver
 from IPython.display import display
 import ipywidgets as widgets
 
-accped_users = {
-    "Adrian": ["18:00", "20:00"],"Sindre":["13:45"]
-}
-reservation_time_and_battery = {"18:00": [67],"20:00":[55],"13:45":[44]}
-
-
+accped_users = {"Adrian": ["18:00", "20:00"], "Sindre": ["13:45"]}
+reservation_time_and_battery = {"18:00": [67], "20:00": [55], "13:45": [44]}
 
 
 class Charger:
-    
+
     # # callback method, called by the button 'switch' when it is pressed
     # def on_value_change(self, change):
     #     if change['new']:
     #         self.stm.send('hand')
     #     else:
     #         self.stm.send('no_hand')
-    
+
     # called by the initial transition
-    def __init__(self):
-        self.idle=
-        self.error=
-        self.connected=
-        self.charging=
-        display(self.yellow, self.checkbox)
-        
     def charger_nozzle_detected(self):
         print("connected")
-        self.connected
 
     def charger_nozzle_disconnected(self):
         print("disconnected")
         self.idle
-    
-    def activate_charger(self,user,time):
-            if (user in accped_users and time in accped_users[user]):
-                print(f"charging until{reservation_time_and_battery[time]} or time is up")
-                self.charged(user,time)
-                self.charging
-            else:
-                print("user has no reserved timeslot")
-                self.connected
-    def charged(self,user,time):
+
+    def activate_charger(self, user, time):
+        if user in accped_users and time in accped_users[user]:
+            print(f"charging until{reservation_time_and_battery[time]} or time is up")
+            self.charged(user, time)
+            self.charging
+        else:
+            print("user has no reserved timeslot")
+            self.connected
+
+    def charged(self, user, time):
         print("time is up")
         print("done charging")
         self.idle
@@ -56,12 +45,13 @@ class Charger:
     def error_resolved(self):
         print("resolved the error")
 
-idle_to_connected= {
+
+idle_to_connected = {
     "source": "initial",
     "target": "connected",
     "effect": "charger_nozzle_detected",
 }
-connected_to_idle= {
+connected_to_idle = {
     "source": "connected",
     "target": "idle",
     "effect": "charger_nozzle_disconnected",
@@ -79,27 +69,44 @@ charging_to_idle = {
     "source": "charging",
     "target": "idle",
 }
-error_to_idle={"trigger": "error_resolved",
+error_to_idle = {
+    "trigger": "error_resolved",
     "source": "error",
     "target": "idle",
-    }
-idle_to_error={"trigger": "error_occur",
+}
+idle_to_error = {
+    "trigger": "error_occur",
     "source": "idle",
     "target": "error",
-    }
-        
-connected_to_error={"trigger": "error_occur",
+}
+
+connected_to_error = {
+    "trigger": "error_occur",
     "source": "connected",
     "target": "error",
-    }
+}
 
-charging_to_error={"trigger": "error_occur",
+charging_to_error = {
+    "trigger": "error_occur",
     "source": "charging",
     "target": "error",
-    }
+}
 charger = Charger()
 
-charger_machine = Machine(transitions=[idle_to_connected,connected_to_charging,connected_to_idle,charging_to_idle,error_to_idle,idle_to_error,charging_to_error,connected_to_error], obj=charger, name="charging_the_car")
+charger_machine = Machine(
+    transitions=[
+        idle_to_connected,
+        connected_to_charging,
+        connected_to_idle,
+        charging_to_idle,
+        error_to_idle,
+        idle_to_error,
+        charging_to_error,
+        connected_to_error,
+    ],
+    obj=charger,
+    name="charging_the_car",
+)
 driver = Driver()
 driver.add_machine(charger_machine)
 driver.start(max_transitions=10)
