6907517a2dd62cd2f39d5432f0be14e705b7c84d
Ask R <askrosand@gmail.com>
2024-04-15 14:07:11 +0200

    Working server

--- src/components/server/run.py
+++ src/components/server/run.py
@@ -1,2 +1,9 @@
+from fastapi import FastAPI
+import uvicorn
+import components.server.crud as crud
+
 def run():
     print("Hello from server")
+    app = FastAPI()
+    app.include_router(crud.router)
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
