a56f0bec6928cec53f1eed745dfe6b61b45ca78e
aldara-iglesias <aldara.iglesias@udc.es>
2024-04-22 15:00:56 +0200

    trying the main

---
+++ src/main.py
@@ -0,0 +1,30 @@
+import sys
+import paho.mqtt.client as mqtt
+import components.car.component_v2 as car
+import components.charger.chargerrun2 as charger
+import components.app.run as app
+import components.server.run as server
+import components.server.mqtt as mqttServer
+
+MQTT_BROKER = "test.mosquitto.org"
+MQTT_PORT = 1883
+
+CAR_TOPIC = "ttm4115/g11/cars"
+CHARGER_TOPIC = "ttm4115/g11/chargers"
+
+app1 = app.App()
+server_mqtt = mqttServer.MQTTClient()
+charger1 = app1.selected_station 
+car1 = app1.car_id
+
+mqtt_client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION1)
+mqtt_client.connect(MQTT_BROKER, MQTT_PORT)
+mqtt_client.subscribe(f"{CAR_TOPIC}/{app1.car_id}")
+server_mqtt.send_start_charging_to_charger(charger1, car1, app1.chosen_percentage)
+server_mqtt.send_start_charging_to_car(charger1, car1)
+
+'''mqtt_client.publish(f"{CAR_TOPIC}/{app1.car_id}", {
+  "charger_id": app1.start_charging,
+  "car_id": app1.car_id,
+  "battery_target": app1.chosen_percentage,
+})'''
