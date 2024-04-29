83ea9f0af5189bb167bd33d450b065095e07a17d
zerath97 <122964950+zerath97@users.noreply.github.com>
2024-04-29 13:32:21 +0200

    Update test_sensehat.py

--- src/components/charger/test_sensehat.py
+++ src/components/charger/test_sensehat.py
@@ -1,22 +1,51 @@
+
 import sensehat as SH
 import time
 
-# This file shows how to enable the SenseHat display.
 
-# ErrorHandler
-def test_error_handler():
-    # Create an instance of ErrorHandler
-    error_handler = SH.ErrorHandler()
-
+# Example of hos to use the SenseHat functions made to display the charger states.
+def test_sensehat():
+    # An instance of the sensehat that starts in the state "init". Can start in other states too.
+    display = SH.Display("init")
+    print(display.state)
     # Start error
-    error_handler.start()
+    display.start()
     
     # Do other tasks...
     time.sleep(2)
     
-    # Stop error
-    error_handler.stop()
+    display.state = "available"
+    print(display.state)
+    time.sleep(5)
+    
+    display.state = "unavailable"
+    print(display.state)
+    time.sleep(2)
+
+    display.battery_cap = 5
+    display.state = "battery status"
+    print(display.state)
+    for i in range(0, display.battery_cap + 1, 1):
+        display.battery = i
+        time.sleep(1)
+    time.sleep(5)
+    
+    display.state = "available"
+    print(display.state)
+    time.sleep(2)
+    
+
+    display.state = "error"
+    print(display.state)
+    time.sleep(3)
+    
+    display.state = "available"
+    print(display.state)
+    time.sleep(5)
+
+    # Remember to stop on exit, as this is a safe way to terminate the sensehat program.
+    display.stop()
 
 
-
-test_error_handler()
+# The function that runs the test.
+test_sensehat()
